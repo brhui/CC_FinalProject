21025ff512446e1e8cf974a26ba7975174740bdd
brhui <chh430@nyu.edu>
2016-11-26 17:35:40 -0500

    Add files via upload

--- FinalProject_ParticleHello/ParticleClass.js
+++ FinalProject_ParticleHello/ParticleClass.js
@@ -1,54 +1,57 @@
+// A simple Particle class
+var system;
 
 var Particle = function(position) {
   this.acceleration = createVector(0, 0.05);
   this.velocity = createVector(random(-1, 1), random(-1, 0));
   this.position = position.copy();
   this.lifespan = 255.0;
-}
+};
 
 Particle.prototype.run = function() {
   this.update();
   this.display();
-}
+};
 
 // Method to update position
-Particle.prototype.update = function() {
+Particle.prototype.update = function(){
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
   this.lifespan -= 2;
-}
+};
 
 // Method to display
 Particle.prototype.display = function() {
-  noStroke();
+  stroke(200, this.lifespan);
+  strokeWeight(2);
   fill(127, this.lifespan);
   ellipse(this.position.x, this.position.y, 12, 12);
-}
+};
 
 // Is the particle still useful?
-Particle.prototype.isDead = function() {
+Particle.prototype.isDead = function(){
   if (this.lifespan < 0) {
     return true;
   } else {
     return false;
   }
-}
+};
 
 var ParticleSystem = function(position) {
   this.origin = position.copy();
   this.particles = [];
-}
+};
 
 ParticleSystem.prototype.addParticle = function() {
   this.particles.push(new Particle(this.origin));
-}
+};
 
 ParticleSystem.prototype.run = function() {
-  for (var i = this.particles.length - 1; i >= 0; i--) {
+  for (var i = this.particles.length-1; i >= 0; i--) {
     var p = this.particles[i];
     p.run();
     if (p.isDead()) {
       this.particles.splice(i, 1);
     }
   }
-}
+};
--- FinalProject_ParticleHello/sketch.js
+++ FinalProject_ParticleHello/sketch.js
@@ -1,49 +1,45 @@
-// Array for Greetings
-var greeting = ["Hello", "Bonjour", "Hola", "Gutentag", "Buongiorno"];
+// Array for storing greetings
+var greeting = ["Hello", "Bonjour", "Hola", "Guten Tag", "Buon Giorno"];
 
-// Variables for the Font
+// Variables for the font
 var font;
-var fontSize = 25;
+var fontSize = 100;
 
 //Variable for switching words
 var newGreeting = 0;
 
+var system;
+
 function preload() {
-  font = loadFont('content/arial.ttf');
+  font = loadFont('content/OpenSans-Bold.ttf');
 }
 
 function setup() {
   createCanvas(800, 600);
   noStroke();
-  fill(255);
 
   textFont(font, fontSize);
+
+  system = new ParticleSystem(createVector(width / 2, 50));
+
 }
 
 function draw() {
-  background(0);
+  background(255);
 
+  system.addParticle();
+  system.run();
+
+  fill(134, 153, 234);
   textAlign(CENTER, CENTER);
   text(greeting[newGreeting], width / 2, height / 2);
-
-  if (mouseIsPressed) {
-    if (mouseButton == LEFT) {
-      newGreeting += 1;
-      if (newGreeting > 4) {
-        newGreeting = 0;
-      }
-    }
-  }
 }
 
-/*
-function mousePressed() {
-  if (mouseButton == LEFT) {
-    for (i = 0; i < greeting.length; i++) {
-      text(greeting[i], 100, 100);
-      if (greeting.length > 2) {
-        i = 0;
-      }
-    }
+// mouseClicked function switches words
+function mouseClicked() {
+  if (newGreeting < 4) {
+    newGreeting += 1;
+  } else {
+    newGreeting = 0;
   }
-} */
+}
