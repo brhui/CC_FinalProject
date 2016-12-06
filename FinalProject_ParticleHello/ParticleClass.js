d0171c0e683f6b6bf8d0b88ed1dce15a0135c85a
brhui <chh430@nyu.edu>
2016-12-06 10:13:21 -0500

    Update ParticleClass.js
    Added pseudo-code.

--- FinalProject_ParticleHello/ParticleClass.js
+++ FinalProject_ParticleHello/ParticleClass.js
@@ -7,8 +7,19 @@ I PLAN ON AUGMENTING THIS TO GET MY PROJECT TO WORK BUT I'M TRYING TO UNDERSTAND
 
 ---------------------------------------------------------------------------------------------------------------- */
 
+/*
+I kind of understand how this works... but not 100%. I put in some comments on what I think things (some are from the example)
+There are a few things in the bottom that I'm not sure about.
 
-// A simple Particle class
+I also am not sure I would get the particles to change colour AND move to form a new word??
+
+How do I even make the particles for a word in the first place??
+
+*/
+
+
+
+// Particle class
 var Particle = function(position) {
   this.acceleration = createVector(0, 0.05);
   this.velocity = createVector(random(-1, 1), random(-1, 0));
@@ -16,19 +27,20 @@ var Particle = function(position) {
   this.lifespan = 400.0;
 };
 
+// Particle .run runs update and display
 Particle.prototype.run = function() {
   this.update();
   this.display();
 };
 
-// Method to update position
+// Updates position of the particle, lifespan (alpha channel) descreases by 2
 Particle.prototype.update = function(){
   this.velocity.add(this.acceleration);
   this.position.add(this.velocity);
   this.lifespan -= 2;
 }
 
-// Method to display
+// Displays particle
 Particle.prototype.display = function() {
   stroke(200, this.lifespan);
   noStroke();
@@ -36,7 +48,7 @@ Particle.prototype.display = function() {
   ellipse(this.position.x, this.position.y, 5, 5);
 };
 
-// Is the particle still useful?
+// Checks to see if the particle is still useful
 Particle.prototype.isDead = function(){
   if (this.lifespan < 0) {
     return true;
@@ -45,15 +57,18 @@ Particle.prototype.isDead = function(){
   }
 }; 
 
+// Particle System class that puts all the particles into an array
 var ParticleSystem = function(position) {
   this.origin = position.copy();
   this.particles = [];
 };
 
+// Particle System .addParticle function that puts new Particles into an array? I think. Not 100% sure what this is for
 ParticleSystem.prototype.addParticle = function() {
   this.particles.push(new Particle(this.origin));
 };
 
+// This .run function checks to see if particles are dead and puts them back into the array? I think... Also not 100% sure on this.
 ParticleSystem.prototype.run = function() {
   for (var i = this.particles.length-1; i >= 0; i--) {
     var p = this.particles[i];
